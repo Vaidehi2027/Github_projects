b9754712d2cec1d09984f722ae79334fa1040caa
Vaidehi Mahale <vaidehimahale180@gmail.com>
2023-04-10 23:45:12 +0530

    first commit

---
+++ Making_pokemon_dashboards.py
@@ -0,0 +1,30 @@
+# import another_module
+# print(another_module.another_variable)
+
+# CREATING OBJECTS USING TURTLE MODULE WHICH IS A GRAPHICS MODULE
+# from turtle import Turtle, Screen
+# # by this we are creating  an object named timmy of class Turtle which we have imported from module turtle
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkRed")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# #  OBJECT ATTRIBUTES CAN BE ACCESSED BY A DOT NOTATION AFTER THE OBJECT
+# print(my_screen.canvwidth)
+# print(my_screen.canvheight)
+#
+# # FUNCTIONS THAT ARE TIED TO AN OBJECT ARE CALLED METHODS
+# my_screen.exitonclick()
+
+# PYTHON PACKAGES
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name",["Pikachu",'Squirtle',"Charmander"])
+# table.add_column("Type",["Electric","Water","Fire"])
+# table.add_column("No_of_likes",[4,5,6])
+# table.align = "l"
+# table.border = True
+# print(table)
+
